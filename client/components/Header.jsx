e321c54578eb5a1b0eb3608b88cb88786d514081
Evan Jones <evanjones125@gmail.com>
2023-02-04 12:34:02 -0500

    add basic structure to Header

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Header = props => {
     return (
-        // flexbox with all the status 
-        <div className="statusBox">
-            <img src="https://avatars.githubusercontent.com/u/94339613?v=4" alt="User profile picture"></img>
+        <div className="headerBox">
+            <img src="https://avatars.githubusercontent.com/u/94339613?v=4" alt="User profile picture" width="80px" height="80px"></img>
+            <p className="statusIcon">&#128512;</p>
+            <p>Looking to fight someone in a Wendy's parking lot</p>
         </div>
     )
 }
