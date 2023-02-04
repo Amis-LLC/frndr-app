0c703123f07a2284dbd0120cd5ab170add0e2b91
Evan Jones <evanjones125@gmail.com>
2023-02-04 11:59:04 -0500

    create header component and render in main app

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,6 @@
 import { hot } from "react-hot-loader/root";
 import React, { Component } from "react";
+import Header from './components/Header';
 // import MainContainer from "./containers/MainContainer.jsx";
 
 class App extends Component {
@@ -8,7 +9,11 @@ class App extends Component {
   }
 
   render() {
-    return <div>{/* <MainContainer /> */}</div>;
+    return (
+      <div>
+        <Header />
+      </div>
+    )
   }
 }
 
---
+++ client/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = props => {
+    return (
+        // flexbox with all the status 
+        <div className="statusBox">
+            <img src="https://avatars.githubusercontent.com/u/94339613?v=4" alt="User profile picture"></img>
+        </div>
+    )
+}
+
+export default Header;
