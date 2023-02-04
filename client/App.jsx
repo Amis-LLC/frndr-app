af83d903c2d4f2103788aa1ad6d4e07cc7d35596
Zahara Aviv <zahara.aviv@gmail.com>
2023-02-04 10:45:54 -0500

    baseline frontend for development + directory structures

---
+++ client/App.jsx
@@ -0,0 +1,15 @@
+import { hot } from "react-hot-loader/root";
+import React, { Component } from "react";
+// import MainContainer from "./containers/MainContainer.jsx";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>{/* <MainContainer /> */}</div>;
+  }
+}
+
+export default hot(App);
