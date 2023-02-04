d7209de1805a23fc3bf9406637369b06e77b0f24
Chanda Gonet <chandagonet@gmail.com>
2023-02-04 14:20:50 -0500

    creates authentication landing page for user

--- client/components/Auth.jsx
+++ client/components/Auth.jsx
@@ -1,57 +1,19 @@
-/**
- * ************************************
- *
- * @module  Auth.js
- * @author  Chanda Gonet
- * @date    2023-02-04
- * @description Sign In/Up
- *
- * ************************************
- */// 
+import React from 'react';
 
- import React from 'react';
- //  import { useSelector, useDispatch } from 'react-redux';
+
+export default function (props) {
+//////////  ---> Hard coding for dev / testing
+const authMode = 'signUp'; 
+// const authMode = 'signUp';
+//////////  <---
  
- 
- function Auth (props) {
-   // const authMode = useSelector((state) => state.auth.authMode);
-   // const dispatch = useDispatch();
- 
-   // const handleChangeAuthMode = () => {
-   //   dispatch(changeAuthMode()); //changeAuthMode lives in the reducer file
-   // }
- 
-   // const handleSignUpSubmit = () => {
-   //   dispatch(signUp()); 
-   //    Try
-   //      - inserts user data into the databse via POST request
-   //      - toggles the sign in, dispatch(changeAuthMode())
-   //    Catch
-   //      - log: `Error signing up: ${err}`
-   //      - render text 'Sorry, sign up failed' to the page in red? (Stretch feature highlighting the error specifically, i.e. username already exists, etc.)
-   // }
- 
-   // const handleSignInSubmit = () => {
-   //  Try
-   //    - find username in db and password in database, compare
-   //    - if matched, redirect to welcome page
-   //    - if not, render text 'Login failed' to the page in red?
-   // }
-   //  Catch
-   //    - log: `Error signing in: ${err}`
-   //    - render text 'Sorry, sign in failed' to the page in red? (Stretch feature highlighting the error specifically, i.e. username already exists, etc.)
- 
- 
-   //////////  ---> Hard coding for dev / testing
-   const authMode = 'signIn'; 
-   // const authMode = 'signUp';
-   //////////  <---
- 
-   if (authMode === 'signIn'){
-     return (
-       <div className="auth_form_container">
+
+
+  if (authMode === 'signIn'){
+
+  return(
+    <div className="auth_form_container">
         <form>
-          {/* <form onSubmit={handleSignInSubmit}> */}
            <div className="auth_form_content">
              <h3>Sign In</h3>
              <div>
@@ -60,7 +22,6 @@
                  // className=""
                  type="text"
                  placeholder="Username"
-                 value={formData.username}
                  />
              </div>
              <div>
@@ -69,7 +30,6 @@
                  // className=""
                  type="password"
                  placeholder="Enter password" 
-                 value={formData.password}
                  />
              </div>
              <div>
@@ -84,22 +44,88 @@
            </div>
          </form>
        </div>
-     )
-   }
- }
- 
- //   if (authMode === 'signUp'){
- //     return (
- //       // Already registered? onClick={changeAuthMode}
- //       ///// SIGN UP FORM 
- //         // name
- //         // username
- //         // phone
- //         // location
- //         // image?
- //     )
- //   }
- // }
- 
- export default Auth;
- 
+    )
+  }
+
+  if (authMode === 'signUp'){
+    return(
+      <div className="auth_form_container">
+          <form>
+             <div className="auth_form_content">
+               <h3>Sign Up</h3>
+  
+               <div>
+                 <label>First name</label>
+                 <input
+                   // className=""
+                   type="text"
+                   placeholder="Your first name"
+                   />
+               </div>
+
+               <div>
+                 <label>Last Name</label>
+                 <input
+                   // className=""
+                   type="text"
+                   placeholder="Your last name"
+                   />
+               </div>
+  
+               <div>
+                 <label>Username</label>
+                 <input
+                   // className=""
+                   type="text"
+                   placeholder="Username"
+                   required
+                   />
+               </div>
+  
+               <div>
+                 <label>Password</label>
+                 <input
+                   // className=""
+                   type="password"
+                   placeholder="Enter password" 
+                   required
+                   />
+               </div>
+  
+               <div>
+                 <label>Phone number</label>
+                 <input
+                   // className=""
+                   type="text"
+                   placeholder="cell phone number"
+                   required
+                   />
+               </div>
+
+               <div>
+                 <label>Location</label>
+                 <select>
+                    <option value="" disabled selected>Select your city</option>
+                    <option value="ladue">Ladue, Missouri</option>
+                </select>
+               </div>
+  
+               <div>
+                 <button type="submit" className="btn btn-primary">
+                   Submit
+                 </button>
+               </div>
+               <p>
+                 {/* //// Stretch feature? */}
+                 <span onClick={handleChangeAuthMode}>I already have an account!</span>
+               </p>
+             </div>
+           </form>
+         </div>
+    )
+  }
+
+
+}
+
+
