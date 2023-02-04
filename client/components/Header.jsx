5bfe3b8ba40a832eb7ef21ae7b64750070541842
Evan Jones <evanjones125@gmail.com>
2023-02-04 16:08:26 -0500

    staging for merge

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,18 +1,5 @@
 import React from 'react';
 
-// const Header = props => {
-//     // grab the profile pic, hobby emoji/icon, and status text from the props and store in a variable?
-//     // const { profile_pic, hobby_icon, status } = props.info;
-    
-//     return (
-//         <div className="headerBox">
-//             <img src="https://avatars.githubusercontent.com/u/94339613?v=4" alt="User profile picture" width="80px" height="80px"></img>
-//             <p className="statusIcon">&#128512;</p>
-//             <p>Looking to fight God in a Wendy's parking lot</p>
-//         </div>
-//     )
-// }
-
 const Header = props => {
     // set up state variable that dictates the visibility of the EmojiList component
     // (testing with a state variable for now because local variables don't persist between renders and because changes to local variables don't trigger re-renders)
