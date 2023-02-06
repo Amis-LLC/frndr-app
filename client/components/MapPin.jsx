6bf60606e6a2e5445f6608834862594e77707591
Evan Jones <evanjones125@gmail.com>
2023-02-06 08:17:33 -0500

    add map pins to page in absolute position

---
+++ client/components/MapPin.jsx
@@ -0,0 +1,32 @@
+/**
+ * ************************************
+ *
+ * @module  MapPin.jsx
+ * @author  Evan
+ * @date
+ * @description  renders a pin from another user on the map
+ *
+ * ************************************
+ */
+
+import React, { useEffect } from "react";
+import Emoji from "./Emoji";
+import { useSelector } from "react-redux";
+
+const MapPin = (props) => {
+  // grab list of users from state
+  const connectionList = useSelector((state) => state.frndr.connectionList);
+
+  const {
+    location,
+    emoji,
+  } = props;
+
+  return (
+    <div className="map-marker">
+      <span className="map-emoji">🍺</span>
+    </div>
+  )
+}
+
+export default MapPin;
--- client/components/WelcomePage.jsx
+++ client/components/WelcomePage.jsx
@@ -8,14 +8,60 @@
  *
  * ************************************
  */
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Header from "./Header";
 import Tile from "./Tile";
+import MapPin from './MapPin';
 import Footer from "./Footer";
+import getFromServer from "../utilities";
+import { setUserMap, setHangoutMap, updateConnectionList } from "../slices";
+import BUTTON_TEXT from "../constants";
 
 const WelcomePage = (props) => {
+  const userMap = useSelector((state) => state.frndr.userMap);
+  const hangoutMap = useSelector((state) => state.frndr.hangoutMap);
+  const connectionList = useSelector((state) => state.frndr.connectionList);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    // on change of props
+    // get feed to set hangoutMap
+    console.log("updating... in useEffect");
+    getFromServer(dispatch, setUserMap, `/api/users`); //get all user info
+    getFromServer(dispatch, setHangoutMap, `/api/hangouts`); // get all hang outs
+  }, [props]);
+  
   const emojiIcon = "👨‍🍳"; // replace with state of emoji.
   // const emojiLabel = "chef"; // replace with state GROWTH
+
+  // store all the map pins
+  const pins = [];
+  for (const _id in hangoutMap) {
+    const hangout = hangoutMap[_id];
+    const user = userMap[hangout.user_id];
+    if (!user) continue;
+    pins.push(
+      <MapPin
+        userID={user._id}
+        hangID={_id}
+        key={_id}
+        phonenumber={user.phonenumber}
+        email={user.email}
+        profilepic={user.picture}
+        location={hangout.location}
+        statusname={hangout.statusname}
+        emoji={hangout.picture}
+        username={hangout.username}
+        buttonText={
+          //random text
+          BUTTON_TEXT[Math.round(Math.random() * (BUTTON_TEXT.length - 1))]
+        }
+        buttonAction={() =>
+          connectButtonClick({ _id, user_id: hangout.user_id })
+        }
+      />
+    );
+  }
 
   return (
     <>
@@ -35,6 +81,7 @@ const WelcomePage = (props) => {
           src={require("../images/map-milford.png")}
           alt="image host"
         />
+        <div className="pins">{pins}</div>
         <Footer />
       </div>
     </>
