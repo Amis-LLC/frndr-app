a2a45c7018cde8f09c097120767f6c30154d2782
EPSparky <ericayeong94@gmail.com>
2023-02-08 13:55:21 -0500

    fixed some auth issues connection with DB

--- server/models/frendr.sql
+++ server/models/frendr.sql
@@ -14,7 +14,7 @@ SET row_security = off;
 
 CREATE TABLE public.Statuses (
 	"_id" serial NOT NULL,
-	"statusName" varchar NOT NULL,
+	"statusName" varchar(255),
     CONSTRAINT "status_pk" PRIMARY KEY ("_id")
 ) WITH (
   OIDS=FALSE
@@ -22,14 +22,14 @@ CREATE TABLE public.Statuses (
 
 CREATE TABLE public.Pictures (
 	"_id" serial NOT NULL,
-	"picture" varchar NOT NULL,
-    "emojiString" varchar NOT NULL,
+	"picture" varchar,
+    "emojiString" varchar,
     CONSTRAINT "picture_pk" PRIMARY KEY ("_id")
 ) WITH (
   OIDS=FALSE
 );
 
-CREATE TABLE public.Location (
+CREATE TABLE public.Locations (
 	"_id" serial NOT NULL,
   "location" varchar NOT NULL,
 	CONSTRAINT "location_pk" PRIMARY KEY ("_id")
@@ -38,12 +38,12 @@ CREATE TABLE public.Location (
   OIDS=FALSE
 );
 
-CREATE TABLE toDoList (
-   _id SERIAL PRIMARY KEY,
-   name VARCHAR(255) NOT NULL,
-   projectId INT NOT NULL,
-   description TEXT NOT NULL,
-FOREIGN KEY (projectId) REFERENCES project(_id));
+-- CREATE TABLE toDoList (
+--    _id SERIAL PRIMARY KEY,
+--    name VARCHAR(255) NOT NULL,
+--    projectId INT NOT NULL,
+--    description TEXT NOT NULL,
+-- FOREIGN KEY (projectId) REFERENCES project(_id));
 
   CREATE TABLE Users (
     "_id" SERIAL PRIMARY KEY,
@@ -53,11 +53,12 @@ FOREIGN KEY (projectId) REFERENCES project(_id));
     email varchar(255) NOT NULL,
     userName varchar(255) NOT NULL,
     password varchar(255) NOT NULL,
-    locationref bigint NOT NULL,
-    statusref bigint NOT NULL,
-    pictureref bigint NOT NULL,
-    hangoutref bigint NOT NULL,
-    FOREIGN KEY (locationref) REFERENCES Location ("_id"),
+    statusname varchar(255),
+    locationref bigint,
+    statusref bigint,
+    pictureref bigint,
+    hangoutref bigint,
+    FOREIGN KEY (locationref) REFERENCES Locations ("_id"),
     FOREIGN KEY (statusref) REFERENCES Statuses ("_id"),
     FOREIGN KEY (pictureref) REFERENCES Pictures ("_id"),
     FOREIGN KEY (hangoutref) REFERENCES Hangout ("_id")
