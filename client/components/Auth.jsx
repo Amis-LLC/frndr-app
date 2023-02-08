f5096171a0ad5b31925c982289d43ce147e39895
Rebecca Anderson <randersonviolin@gmail.com>
2023-02-08 14:48:26 -0500

    add in functioning websockets, currently cannot login

--- client/components/Auth.jsx
+++ client/components/Auth.jsx
@@ -23,6 +23,15 @@ import {
   setThePage,
 } from "../slices";
 
+// websocket testing ->
+import { io } from "socket.io-client";
+const socket = io();
+
+socket.emit("chat message", "testing the socket connection");
+socket.on("chat message", (msg) => {
+  console.log("client-side chat message received: ", msg);
+});
+
 //  user db schema
 // {
 //   firstName: ' ',
